4c0d0e49381dd8ff3f482101884a26b939d6782d
Nate Harris <n8gr8gbln@gmail.com>
2020-12-08 20:53:26 -0700

    Fix README reading issue, bump to v1.0.2

--- ipify2/__info__.py
+++ ipify2/__info__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 __title__ = "ipify2"
 __author__ = 'Nate Harris'
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import Command, setup
 
 import ipify2.__info__ as package_info
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 with open("requirements.txt", 'r') as fh:
